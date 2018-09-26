43dcb71f7d048620e76a94a6a9b653c130b74b06
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2018-09-26 17:22:37 -0500

    Added general design layout

---
+++ floodMax/src/floodMax/Message.java
@@ -0,0 +1,80 @@
+package floodMax;
+
+public class Message {
+
+  public enum MessageType {
+    NACK, REJECT, IAMLEADER, EXPLORE;
+  }
+
+  MessageType mType;
+  int uid;
+  int max_uid;
+  String status;
+  int round;
+
+  public Message() {
+    // TODO Auto-generated constructor stub
+  }
+  Message(int uid, int max_uid, String status) {
+    this.uid = uid;
+    this.max_uid = max_uid;
+    this.status = status;
+  }
+
+  public int getUid() {
+    return uid;
+  }
+
+  public void setUid(int uid) {
+    this.uid = uid;
+  }
+
+  public int getMax_uid() {
+    return max_uid;
+  }
+
+  public void setMax_uid(int max_uid) {
+    this.max_uid = max_uid;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public int getRound() {
+    return round;
+  }
+
+  public void setRound(int round) {
+    this.round = round;
+  }
+
+  void set_round(int round) {
+    this.round = round;
+  }
+
+  int get_uid() {
+    return uid;
+  }
+
+  int get_max_uid() {
+    return max_uid;
+  }
+
+  String get_status() {
+    return status;
+  }
+
+  public MessageType getmType() {
+    return mType;
+  }
+
+  public void setmType(MessageType mType) {
+    this.mType = mType;
+  }
+
+}
